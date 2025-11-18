4a29f38e452b0c902ad62def1f3b6e9c7cfc4c71
fareenchitti-afk <fareenchitti@gmail.com>
2025-11-18 14:06:38 +0530

    8.input format output format.py

---
+++ Course work/8.input format output format.py
@@ -0,0 +1,29 @@
+s=input("Python") #string
+print(s)
+
+i=int(input()) #integer
+
+i=float(input()) #float
+
+l=list(map(int,input().split(','))) #list 
+
+name=input("Enter name:")
+print(name)
+age=int(input("Enter age:"))
+print(age)
+
+names=input()
+ruchitha, preethi, varsha
+print(names)
+
+names=list(map(int,input().split()))
+print(names)
+
+names=tuple(input().split())#tuple input for str
+print(names)
+
+names=set(map(int,input().split())) #set input()
+print(names)
+
+d=eval(input())
+{'name':'rama','age':21}
